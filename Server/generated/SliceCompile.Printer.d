7d9e03b552fca852d7e77d9c0c9962bcd757d702
verm-v <verm-v@yandex.ru>
2021-05-11 14:44:18 +0600

    Initial commit

---
+++ Server/generated/SliceCompile.Printer.d
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<dependencies>
+  <source name="Printer.ice">
+    <options>
+      <IceHome>D:\Work\Projects\Console\Zero-Ice\packages\zeroc.ice.net.3.7.5\build\..</IceHome>
+      <IceToolsPath>D:\Work\Projects\Console\Zero-Ice\packages\zeroc.ice.net.3.7.5\build\..\tools</IceToolsPath>
+      <OutputDir>D:\Work\Projects\Console\Zero-Ice\Server\generated</OutputDir>
+    </options>
+  </source>
+</dependencies>
